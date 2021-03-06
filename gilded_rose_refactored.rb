81638ad2d0c7ce2986cb7c9fae27a90f692ccc1a
Angelo <goalista@gmail.com>
2021-03-06 12:28:28 +0800

    refactored

---
+++ gilded_rose_refactored.rb
@@ -0,0 +1,78 @@
+def update_quality(items)
+    items.each do |item|
+        # BEAR IN MIND EXCEPTIONS ARE STILL UNDER GENERAL RULES EXCEPT LEGENDARY ITEMS
+
+        # EXCEPTIONS
+        # “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+        if item.name != 'Sulfuras, Hand of Ragnaros'
+
+            # “Aged Brie” actually increases in Quality the older it gets
+            if item.name == 'Aged Brie'
+                item.quality += 2
+            
+                # Once the sell by date has passed, Quality degrades twice as fast
+                if item.sell_in <= 0
+                    item.quality += 2
+                end
+            end
+
+            # “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches
+            if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+                item.quality += 2
+
+                # Quality increases by 2 when there are 10 days or less
+                if item.sell_in <= 10
+                    item.quality += 1
+
+                    # by 3 when there are 5 days or less 
+                    if item.sell_in <= 5
+                        item.quality += 1
+
+                        # Quality drops to 0 after the concert
+                        if item.sell_in <= 0
+                            item.quality = 0
+                        end
+                    end
+                end
+            end
+            
+            # THE GENERAL RULE
+            # Once the sell by date has passed, Quality degrades twice as fast     
+            if item.sell_in <= 0
+                item.quality -= 1
+            end
+
+            # At the end of each day our system lowers both values for every item
+            item.sell_in -= 1
+            item.quality -= 1   
+
+            # CORRECTIONS
+            # The Quality of an item is never more than 50
+            if item.quality > 50
+                item.quality = 50
+            end
+            
+            # The Quality of an item is never negative
+            if item.quality < 0
+                item.quality = 0
+            end
+        end
+    end
+end
+  
+  # DO NOT CHANGE THINGS BELOW -----------------------------------------
+  
+  Item = Struct.new(:name, :sell_in, :quality)
+  
+  # We use the setup in the spec rather than the following for testing.
+  #
+  # Items = [
+  #   Item.new("+5 Dexterity Vest", 10, 20),
+  #   Item.new("Aged Brie", 2, 0),
+  #   Item.new("Elixir of the Mongoose", 5, 7),
+  #   Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+  #   Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  #   Item.new("Conjured Mana Cake", 3, 6),
+  # ]
+  
+  
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,4 +1,4 @@
-require_relative './gilded_rose'
+require_relative './gilded_rose_refactored'
 
 RSpec.describe "GildedRose" do
   let!(:items) do
@@ -11,4 +11,71 @@ RSpec.describe "GildedRose" do
      Item.new("Conjured Mana Cake", 3, 6),
     ]
   end
+
+  it "changes the first item to have 9 and 19 as values respectively" do
+    expect(update_quality(items)[0]).to eq(Item.new("+5 Dexterity Vest", 9, 19))
+  end
+
+  it "changes the second item to have 1 and 1 as values respectively" do
+    expect(update_quality(items)[1]).to eq(Item.new("Aged Brie", 1, 1))
+  end
+
+  it "changes the third item to have 4 and 6 as values respectively" do
+    expect(update_quality(items)[2]).to eq(Item.new("Elixir of the Mongoose", 4, 6))
+  end
+
+  it "does not change the fourth item" do
+    expect(update_quality(items)[3]).to eq(Item.new("Sulfuras, Hand of Ragnaros", 0, 80))
+  end
+
+  it "changes the fifth item to have 14 and 21 as values respectively" do
+    expect(update_quality(items)[4]).to eq(Item.new("Backstage passes to a TAFKAL80ETC concert", 14, 21))
+  end
+
+  it "changes the sixth item to have 14 and 21 as values respectively" do
+    expect(update_quality(items)[5]).to eq(Item.new("Conjured Mana Cake", 2, 5))
+  end
+
+  context "normal degrading items" do
+    let!(:normal_items) do
+      [
+       Item.new("+5 Dexterity Vest", 10, 20),
+       Item.new("Elixir of the Mongoose", 5, 7),
+      ]
+    end
+    it "should degrade normally" do 
+      update_quality(normal_items)
+      expect(normal_items[0]).to eq(Item.new("+5 Dexterity Vest", 9, 19))
+      expect(normal_items[1]).to eq(Item.new("Elixir of the Mongoose", 4, 6))
+    end
+  end
+
+  context "inverse degrading items" do
+    let!(:inverse_items) do
+      [
+        Item.new("Aged Brie", 2, 0),
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      ]
+    end
+    it "should upgrade instead" do 
+      update_quality(inverse_items)
+      expect(inverse_items[0]).to eq(Item.new("Aged Brie", 1, 1))
+      expect(inverse_items[1]).to eq(Item.new("Backstage passes to a TAFKAL80ETC concert", 14, 21))
+    end
+  end
+
+  context "permanent quality items" do
+    let!(:permanent_items) do
+      [
+        Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+      ]
+    end
+    it "should remain in quality" do 
+      update_quality(permanent_items)
+      expect(permanent_items[0]).to eq(Item.new("Sulfuras, Hand of Ragnaros", 0, 80))
+    end
+  end
+
 end
+
+
